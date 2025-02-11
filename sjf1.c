134a74f659d8cbdcb7a8f568770d0c2ddb6d2033
merindaniel04 <merindaniel25@gmail.com>
2025-02-11 16:26:49 +0530

    Add files via upload

--- sjf1.c
+++ sjf1.c
@@ -1,36 +1,58 @@
 #include<stdio.h>
 void main()
 {
- int n,i,j;
+ int n,i,j,temp;
  double avg_wait=0,avg_tat=0;
  printf("Enter the number of processes: ");
  scanf("%d",&n);
- int btime[n],wtime[n],tattime[n];
+ int btime[n],wtime[n],tattime[n],p[n];
  printf("Enter the burst time of the processes\n");
  for(i=0;i<n;i++)
  {
-   printf("P%d Burst Time: ",(i+1));
+ p[i]=i+1;
+   printf("P%d Burst Time: ",p[i]);
    scanf("%d",&btime[i]);
    printf("\n");
  }
- wtime[0]=0;
- for(i=0;i<n;i++)
+for(i=0;i<n-1;i++)
+{
+ for(j=i+1;j<n;j++)
  {
-  wtime[i]=wtime[i-1]+btime[i-1];
-  avg_wait=avg_wait+wtime[i];
+  if(btime[i]>btime[j])
+  {
+   temp=btime[i];
+   btime[i]=btime[j];
+   btime[j]=temp;
+   temp=p[i];
+   p[i]=p[j];
+   p[j]=temp;
+  }
+  if(btime[i]>btime[j])
+  {
+     temp=p[i];
+   p[i]=p[j];
+   p[j]=temp;
+   }
  }
- avg_wait=avg_wait/n;
- for(i=0;i<n;i++)
+}
+wtime[0]=0;
+for(i=0;i<n;i++)
+{
+    wtime[i]=wtime[i-1]+btime[i-1];
+    avg_wait=avg_wait+wtime[i];
+}
+avg_wait=avg_wait/n;
+  for(i=0;i<n;i++)
  {
   tattime[i]=wtime[i]+btime[i];
-  avg_tat=avg_tat+tattime[i];
+   avg_tat=avg_tat+tattime[i];
  }
  avg_tat=avg_tat/n;
  
  printf("Process\t Burst Time\t Wait Time\t Turn Around Time\n");
  for(i=0;i<n;i++)
   {
-   printf("P%d\t%d\t\t%d\t\t%d\n",i+1,btime[i],wtime[i],tattime[i]);
+   printf("P%d\t%d\t\t%d\t\t%d\n",p[i],btime[i],wtime[i],tattime[i]);
   }
  printf("\n");
  printf("Average Waiting Time: %.2f\nAverage Turn Around Time : %.2f\n", avg_wait,avg_tat);
@@ -43,7 +65,7 @@ void main()
  printf("\n|");
  for(i=0;i<n;i++)
  {
-  printf("P%d     |",i+1);
+  printf("P%d     |",p[i]);
  }
  printf("\n|");
  for(i=0;i<n;i++)
@@ -60,5 +82,3 @@ void main()
  printf("\n");
  }
  
-  
-   
